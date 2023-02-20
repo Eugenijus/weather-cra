d7e047d193d11487cef6c674a1a8110d2d01396a
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2023-02-21 01:27:14 +0200

    updates fetch information and adds weather layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { format } from 'date-fns';
-import './App.css';
+
+import { weatherCodes } from './weatherCodes';
+import './Weather.css';
 
 function App() {
-  const [weather, setWeather] = useState({});
+  const [weatherCode, setWeatherCode] = useState(0);
+  const [currentWeather, setCurrentWeather] = useState({});
   const [temp, setTemp] = useState(0);
   const [dayAndHour, setDayAndHour] = useState('');
   const [latitude, setLatitude] = useState(54.69);
@@ -17,13 +20,15 @@ function App() {
           latitude +
           '&longitude=' +
           longitude +
-          '&hourly=temperature_2m'
+          '&current_weather=true&hourly=temperature_2m,weathercode'
       )
       .then((res) => {
         const {
-          hourly: { time, temperature_2m },
+          hourly: { time, temperature_2m, weathercode },
           hourly_units: { temperature_2m: unit },
+          current_weather,
         } = res.data;
+        setCurrentWeather(current_weather);
 
         const date = new Date();
         const onlyDate = format(date, 'yyyy-MM-dd');
@@ -32,16 +37,28 @@ function App() {
         setDayAndHour(timeTarget);
         const index = time.findIndex((t) => t === timeTarget);
         setTemp(temperature_2m[index] + unit);
-        setWeather(res.data);
+        setWeatherCode(weathercode[index]);
       })
       .catch((error) => console.error(error));
   }, [latitude, longitude]);
 
+  const weatherCodeText = weatherCodes.get(weatherCode);
   return (
-    <div className='App'>
-      <header>weather</header>
-      <div>{dayAndHour}</div>
-      <div>{temp}</div>
+    <div className='weather-app-container'>
+      <div className='weather-app'>
+        <div className='weather-header'>
+          <h1>Weather App</h1>
+        </div>
+        <div className='weather-body'>
+          <div className='weather-icon'></div>
+          <div className='weather-details'>
+            <h2>Vilnius</h2>
+            <p>Temperature: {temp}</p>
+            <p>Weather: {weatherCodeText}</p>
+            <p>Day: {dayAndHour}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
