0ec09753380f276046daf8b4012feb91c1524570
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2023-02-28 02:59:59 +0200

    gets location from client browser and fetches data from localhost server

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,61 +1,92 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { format } from 'date-fns';
-
-import { weatherCodes } from './weatherCodes';
 import './Weather.css';
 
 function App() {
-  const [weatherCode, setWeatherCode] = useState(0);
   const [currentWeather, setCurrentWeather] = useState({});
-  const [temp, setTemp] = useState(0);
-  const [dayAndHour, setDayAndHour] = useState('');
-  const [latitude, setLatitude] = useState(54.69);
-  const [longitude, setLongitute] = useState(25, 28);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitute] = useState(0);
+
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLatitude(position.coords.latitude);
+        setLongitute(position.coords.longitude);
+      });
+    } else {
+      console.log('Geolocation Not Available');
+    }
+  }, []);
 
   useEffect(() => {
     axios
       .get(
-        'https://api.open-meteo.com/v1/forecast?latitude=' +
+        'http://localhost:8080/weather?' +
+          'lat=' +
           latitude +
-          '&longitude=' +
-          longitude +
-          '&current_weather=true&hourly=temperature_2m,weathercode'
+          '&lon=' +
+          longitude
       )
       .then((res) => {
-        const {
-          hourly: { time, temperature_2m, weathercode },
-          hourly_units: { temperature_2m: unit },
-          current_weather,
-        } = res.data;
-        setCurrentWeather(current_weather);
-
-        const date = new Date();
-        const onlyDate = format(date, 'yyyy-MM-dd');
-        const onlyHour = format(date, 'HH:00');
-        const timeTarget = onlyDate + 'T' + onlyHour;
-        setDayAndHour(timeTarget);
-        const index = time.findIndex((t) => t === timeTarget);
-        setTemp(temperature_2m[index] + unit);
-        setWeatherCode(weathercode[index]);
+        setCurrentWeather(res.data);
       })
       .catch((error) => console.error(error));
   }, [latitude, longitude]);
 
-  const weatherCodeText = weatherCodes.get(weatherCode);
+  const handleLatitudeChange = (event) => {
+    const { value } = event.target;
+    setLatitude(value);
+  };
+
+  const handleLongitudeChange = (event) => {
+    const { value } = event.target;
+    setLongitute(value);
+  };
+
+  const { temperature = 0.0, time = '', weatherText = '' } = currentWeather;
+  const onlyDate = time.split('T')[0];
   return (
     <div className='weather-app-container'>
       <div className='weather-app'>
         <div className='weather-header'>
           <h1>Weather App</h1>
         </div>
+        <div className='weather-input'>
+          <div>
+            <label>
+              Latitude:{' '}
+              <input
+                value={latitude}
+                onChange={handleLatitudeChange}
+                min='-90'
+                max='90'
+                size='15'
+                type='number'
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Longitude:{' '}
+              <input
+                value={longitude}
+                onChange={handleLongitudeChange}
+                min='-180'
+                max='180'
+                size='15'
+                type='number'
+              />
+            </label>
+          </div>
+        </div>
         <div className='weather-body'>
           <div className='weather-icon'></div>
           <div className='weather-details'>
-            <h2>Vilnius</h2>
-            <p>Temperature: {temp}</p>
-            <p>Weather: {weatherCodeText}</p>
-            <p>Day: {dayAndHour}</p>
+            {/* <h2>Vilnius</h2> */}
+            <h2>{temperature || 0}°C</h2>
+            <p>Day: {onlyDate}</p>
+            <p>Weather: {weatherText}</p>
           </div>
         </div>
       </div>
